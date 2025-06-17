887c40e7ae3575da4020f9a67a1cfa83c42d3770
Luan Eto <luan.eto18@gmail.com>
2025-06-16 21:38:42 -0300

    Fix process mode pause button

--- 2. Breakout/breakout/scripts/game/ui/buttons/pause/pause_button.gd
+++ 2. Breakout/breakout/scripts/game/ui/buttons/pause/pause_button.gd
@@ -2,13 +2,18 @@ extends Button
 class_name PauseButton
 
 
+func _ready() -> void:
+	# Set the process mode depending on the signal
+	GameState.paused_game.connect(func(): process_mode = Node.PROCESS_MODE_WHEN_PAUSED)
+	GameState.start_game.connect(func(): process_mode = Node.PROCESS_MODE_INHERIT)
+
+
 func _pressed() -> void:
+	print("apertou", GameState.current_state)
 	if GameState.current_state == GameState.GAME_STATES.STARTED:
-		process_mode = Node.PROCESS_MODE_WHEN_PAUSED	
-		GameState.set_game_state(GameState.GAME_STATES.PAUSED)	
 		get_tree().set_pause(true)
+		GameState.set_game_state(GameState.GAME_STATES.PAUSED)	
 		
 	elif GameState.current_state == GameState.GAME_STATES.PAUSED:
 		get_tree().set_pause(false)		
 		GameState.set_game_state(GameState.GAME_STATES.STARTED)
-		process_mode = Node.PROCESS_MODE_INHERIT
